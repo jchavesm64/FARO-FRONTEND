d2ebd3e81e30f2b76d01327f4eec90616b70c162
Hernán Alfaro Araya <55323501+KuroScar42@users.noreply.github.com>
2025-01-30 08:42:41 -0600

    Removing unused variables | Formatting code

--- src/Pages/Reception/InHouse/InHouse.js
+++ src/Pages/Reception/InHouse/InHouse.js
@@ -1,21 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Row, Card, CardHeader, FormGroup, Label, CardTitle, CardBody, Badge, Input, Button, Col } from 'reactstrap';
-import Breadcrumbs from '../../../components/Common/Breadcrumb';
+import React from "react";
+import { Container } from "reactstrap";
+import Breadcrumbs from "../../../components/Common/Breadcrumb";
 
 const InHouse = () => {
-
-    document.title = "InHouse | FARO";
-    return (
-        <React.Fragment>
-            <div className="page-content">
-                <Container fluid={true}>
-                    <Breadcrumbs title="InHouse" breadcrumbItem="Recepción" breadcrumbItemUrl="/reception" />
-                </Container>
-            </div>
-        </React.Fragment>
-
-
-    );
+  document.title = "InHouse | FARO";
+  return (
+    <div className="page-content">
+      <Container fluid={true}>
+        <Breadcrumbs
+          title="InHouse"
+          breadcrumbItem="Recepción"
+          breadcrumbItemUrl="/reception"
+        />
+      </Container>
+    </div>
+  );
 };
 
 export default InHouse;
